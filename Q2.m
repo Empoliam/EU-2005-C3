2fd80cf2f14b2ff61dfb3d05d26337e305f03cfd
Empoliam <liamjwright@btinternet.com>
2020-03-02 15:55:26 +0000

    Added q2

---
+++ Q2.m
@@ -0,0 +1,26 @@
+clear();
+
+a = 2e-4;
+T = 30;
+d = 0.3/60;
+Nf = 100;
+k = 1/60;
+
+dL = @(l) a .* k .* Nf .* (T-l) - d;
+
+tMax = 10000;
+dt = 1e-1;
+t = 0:dt:tMax;
+
+L = zeros(1,length(t));
+L(1) = 0.001;
+
+i = 1;
+while i < length(t)
+
+    L(i+1) = L(i) + dt*dL(L(i));
+    i = i + 1;
+    
+end
+
+plot(t,L)
